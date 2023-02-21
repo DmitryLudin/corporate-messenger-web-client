aa2339dc9989e44eb530745c1c078aae90210a2b
Лудин Дмитрий Валерьевич <ludin@tochka.com>
2023-02-21 22:02:18 +0500

    feat: доработки сайдбара

--- src/modules/main-sidebar/components/list-item.tsx
+++ src/modules/main-sidebar/components/list-item.tsx
@@ -1,13 +1,18 @@
+import { DeleteForever, Edit } from '@mui/icons-material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {
   IconButton,
+  ListDivider,
   ListItem,
   ListItemButton,
   ListItemContent,
   ListItemDecorator,
+  Menu,
+  MenuItem,
+  Tooltip,
   Typography,
 } from '@mui/joy';
-import React from 'react';
+import { MouseEventHandler, useState } from 'react';
 
 interface IProps {
   text: string;
@@ -22,17 +27,72 @@ export function MainSidebarListItem({
   onClick,
   selected = false,
 }: IProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLAnchorElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <ListItem
+      sx={{
+        '&:hover': {
+          '& .MuiIconButton-root': {
+            display: 'flex',
+          },
+        },
+      }}
       endAction={
-        <IconButton
-          sx={{ '--IconButton-size': '24px' }}
-          size="sm"
-          color="neutral"
-          variant="plain"
-        >
-          <MoreVertIcon fontSize="small" />
-        </IconButton>
+        <>
+          <Tooltip title="Настройка канала" size="sm" arrow placement="right">
+            <IconButton
+              id="positioned-demo-button"
+              aria-controls={open ? 'positioned-demo-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              sx={{
+                '--IconButton-size': '24px',
+                display: open ? 'flex' : 'none',
+              }}
+              size="sm"
+              color="neutral"
+              variant="plain"
+            >
+              <MoreVertIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            id="positioned-demo-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            size="sm"
+            aria-labelledby="positioned-demo-button"
+            placement="top-start"
+          >
+            <MenuItem onClick={handleClose}>
+              <ListItemDecorator>
+                <Edit />
+              </ListItemDecorator>
+              Edit post
+            </MenuItem>
+            <MenuItem disabled onClick={handleClose}>
+              <ListItemDecorator />
+              Draft post
+            </MenuItem>
+            <ListDivider />
+            <MenuItem onClick={handleClose} variant="soft" color="danger">
+              <ListItemDecorator sx={{ color: 'inherit' }}>
+                <DeleteForever />
+              </ListItemDecorator>{' '}
+              Delete
+            </MenuItem>
+          </Menu>
+        </>
       }
     >
       <ListItemButton
